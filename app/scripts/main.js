6c0ed89195e4d29727874a712c9779db1b17a7ac
BrianNewsom <brian.newsom@colorado.edu>
2014-11-26 14:45:02 -0700

    really broken ambient chaos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -33,7 +33,15 @@ var conn = new WebSocket("ws://" + atlasTrafficServer + ":8765");
 var output = '';
 conn.onopen = function (ev) { return; };
 
-conn.onmessage = function (ev) {
+//conn.onmessage = function (ev) {
+    var pattern = new sc.Pshuf(sc.series(12), Infinity);
+    var scale   = new sc.Scale.major();
+    var chords  = [
+      [0, 1, 4], [0, 1, 5], [0, 1, 6],
+      [0, 2, 6], [0, 2, 5], [0, 2, 4],
+      [0, 3, 6], [0, 3, 5], [0, 3, 4]
+    ];
+
     var msec = timbre.timevalue("BPM120 L16");
     var osc  = T("saw");
     var env  = T("env", {table:[0.2, [1, msec * 48], [0.2, msec * 16]]});
@@ -45,6 +53,15 @@ conn.onmessage = function (ev) {
     synth = T("+saw", {freq:(msec * 2)+"ms", add:0.5, mul:0.85}, synth);
     synth = T("lpf" , {cutoff:800, Q:12}, synth);
     synth = T("reverb", {room:0.95, damp:0.1, mix:0.75}, synth);
+
+    T("interval", {interval:msec * 64}, function() {
+          var root = pattern.next();
+            chords.choose().forEach(function(i) {
+                    gen.noteOn(scale.wrapAt(root + i) +60, 80);
+                      });
+                        pan.pos.value = Math.random() * 2 - 1;
+    }).set({buddies:synth}).start();
+
     /*
     timbre.rec(function(output) {
         var pluck = T("pluck", {freq:jingleBells.notes[noteIterator()], mul:0.5}).bang();
@@ -87,5 +104,5 @@ conn.onmessage = function (ev) {
           ).play();
     });
     */
-};
+//};
 
