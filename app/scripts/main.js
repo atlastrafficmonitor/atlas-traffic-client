aa83ec0fb1ba3b0bb62893e7289dab7ffee5691e
BrianNewsom <brian.newsom@colorado.edu>
2014-11-26 13:59:32 -0700

    add code that only works once. working on getting it to play on each event (long way to go)

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -28,12 +28,46 @@ var jingleBells = {
 
 var noteIterator = NoteIterator(jingleBells);
 
-var atlasTrafficServer = '192.168.50.4'
+var atlasTrafficServer = '0.0.0.0'
 var conn = new WebSocket("ws://" + atlasTrafficServer + ":8765");
-
+var output = '';
 conn.onopen = function (ev) { return; };
 
 conn.onmessage = function (ev) {
-  T("pluck", {freq:jingleBells.notes[noteIterator()], mul:0.5}).bang().play();
-  console.log(ev);
+    /*
+    timbre.rec(function(output) {
+        var pluck = T("pluck", {freq:jingleBells.notes[noteIterator()], mul:0.5}).bang();
+        output.send(pluck);
+    }).then(function(result) {
+        var L = T("buffer", {buffer:result, loop:false});
+        T("reverb", {room:0.9, damp:0.2, mix:0.45},
+            T("pan", {pos:-0.6}, L), T("pan", {pos:+0.6}, L)
+        ).play();
+    })
+    */
+    timbre.rec(function(output) {
+        var midis = [69, 71, 72, 76, 69, 71, 72, 76]//.scramble();
+        var msec  = timbre.timevalue("bpm120 l8");
+        var synth = T("OscGen", {env:T("perc", {r:msec, ar:true})});
+        T("interval", {interval:msec}, function(count) {
+            if (count < midis.length) {
+                synth.noteOn(midis[count], 100);
+            } else {
+                output.done();
+            }
+        }).start();
+
+        output.send(synth);
+        }).then(function(result) {
+          var L = T("buffer", {buffer:result, loop:true});
+          var R = T("buffer", {buffer:result, loop:true});
+          var num = 400;
+          var duration = L.duration;
+          R.pitch = (duration * (num - 1)) / (duration * num);
+          T("reverb", {room:0.9, damp:0.2, mix:0.45},
+          //T("delay", {time:"bpm120 l16", fb:0.1, cross:true},
+              T("pan", {pos:-0.6}, L), T("pan", {pos:+0.6}, R)
+          ).play();
+    });
 };
+
